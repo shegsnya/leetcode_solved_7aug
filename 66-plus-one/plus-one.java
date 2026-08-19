79615c833099585dba43f645c57bb816079e6ccc
shegsnya <shagunarya1110@gmail.com>
2026-08-19 23:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (8.53%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int[] arr=new int[digits.length+1];
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            else{
+                digits[i]=0;
+                arr[i]=0;
+            }
+        }
+        arr[0]=1;
+        return arr;
+    }
+}
