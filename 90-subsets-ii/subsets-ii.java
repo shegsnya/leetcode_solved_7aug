283499c54f32841ca620468c30c461a2aa812317
shegsnya <shagunarya1110@gmail.com>
2026-09-01 19:47:12 +0530

    Time: 3 ms (54.75%) | Memory: 45.1 MB (60.06%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        subsettwo(0,nums,ans,new ArrayList<>());
+        return ans;
+    }
+    static void subsettwo(int index,int[] nums,List<List<Integer>> ans,ArrayList<Integer> ds){
+        ans.add(new ArrayList<>(ds));
+        for(int i=index;i<nums.length;i++){
+            if(i!=index && nums[i]==nums[i-1]){
+                continue;
+            }
+            ds.add(nums[i]);
+            subsettwo(i+1,nums,ans,ds);
+            ds.remove(ds.size()-1);
+
+        }
+
+    }
+}
